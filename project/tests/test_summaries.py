7306f3936a5f3ee23fc6eb14d9a2bdfc4f76e9ff
Francisco De La Cruz <helloquico@gmail.com>
2025-07-12 00:16:16 +0200

    Fix: DeprecationWarning: Use 'content=<...>'

--- project/tests/test_summaries.py
+++ project/tests/test_summaries.py
@@ -3,7 +3,7 @@ import json
 
 def test_create_summary(test_app_with_db):
     response = test_app_with_db.post(
-        "/summaries/", data=json.dumps({"url": "https://foo.bar"})
+        "/summaries/", content=json.dumps({"url": "https://foo.bar"})
     )
     assert response.status_code == 201
     assert response.json()["url"] == "https://foo.bar"
