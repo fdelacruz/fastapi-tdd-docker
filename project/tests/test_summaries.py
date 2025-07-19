083d32dc3a2c21a78c7426aced8ec53c6ce0bd19
Francisco De La Cruz <helloquico@gmail.com>
2025-07-19 18:48:03 +0000

    Update test_create_summary to accept normalized URLs

--- project/tests/test_summaries.py
+++ project/tests/test_summaries.py
@@ -2,11 +2,13 @@ import json
 
 
 def test_create_summary(test_app_with_db):
+    input_url = "https://foo.bar"
     response = test_app_with_db.post(
-        "/summaries/", content=json.dumps({"url": "https://foo.bar"})
+        "/summaries/", content=json.dumps({"url": input_url})
     )
     assert response.status_code == 201
-    assert response.json()["url"] == "https://foo.bar"
+    returned_url = response.json()["url"]
+    assert returned_url.rstrip("/") == input_url
 
 
 def test_create_summaries_invalid_json(test_app):
