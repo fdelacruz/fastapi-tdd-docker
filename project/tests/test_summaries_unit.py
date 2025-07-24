e62c84addeaacba82170c9c184f60dfd6c84f6b2
Francisco De La Cruz <helloquico@gmail.com>
2025-07-24 22:27:39 +0200

    Update Summary

--- project/tests/test_summaries_unit.py
+++ project/tests/test_summaries_unit.py
@@ -3,7 +3,7 @@ from datetime import datetime
 
 import pytest
 
-from app.api import crud, summaries
+from app.api import crud
 
 
 def test_create_summary(test_app, monkeypatch):
@@ -135,7 +135,25 @@ def test_remove_summary_incorrect_id(test_app, monkeypatch):
 
 
 def test_update_summary(test_app, monkeypatch):
-    pass
+    test_request_payload = {"url": "https://foo.bar", "summary": "updated"}
+    test_response_payload = {
+        "id": 1,
+        "url": "https://foo.bar",
+        "summary": "summary",
+        "created_at": datetime.utcnow().isoformat(),
+    }
+
+    async def mock_put(id, payload):
+        return test_response_payload
+
+    monkeypatch.setattr(crud, "put", mock_put)
+
+    response = test_app.put(
+        "/summaries/1/",
+        data=json.dumps(test_request_payload),
+    )
+    assert response.status_code == 200
+    assert response.json() == test_response_payload
 
 
 @pytest.mark.parametrize(
@@ -198,7 +216,14 @@ def test_update_summary(test_app, monkeypatch):
 def test_update_summary_invalid(
     test_app, monkeypatch, summary_id, payload, status_code, detail
 ):
-    pass
+    async def mock_put(id, payload):
+        return None
+
+    monkeypatch.setattr(crud, "put", mock_put)
+
+    response = test_app.put(f"/summaries/{summary_id}/", data=json.dumps(payload))
+    assert response.status_code == status_code
+    assert response.json()["detail"] == detail
 
 
 def test_update_summary_invalid_url(test_app):
